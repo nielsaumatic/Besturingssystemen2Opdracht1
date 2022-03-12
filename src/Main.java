739e80445b77e1c998859982c2d47842dcadea0d
nielsaumatic <niels.de.walsche@gmail.com>
2022-03-12 13:18:39 +0100

    fcfs main structuur

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Process> processes = XML.readXML("test.xml");
+
+        Scheduler fcfs = new FCFS(processes);
     }
 
 }
