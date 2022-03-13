cc3f9e4c75eb0a32d904c863cc81b541cebdbd95
nielsaumatic <niels.de.walsche@gmail.com>
2022-03-13 17:00:24 +0100

    srt start

---
+++ src/SRT.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class SRT extends Scheduler{
+
+    public SRT(List<Process> processes) {
+        super(processes);
+    }
+
+    @Override
+    public void schedule() {
+
+    }
+}
